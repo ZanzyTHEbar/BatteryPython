9daa238075790e29d32bd543a780e9043e43caf7
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2022-12-29 04:26:46 +0000

    update - fix math

--- src/calculate_battery_connections.py
+++ src/calculate_battery_connections.py
@@ -4,7 +4,7 @@ class CalculateBatteryConnections:
         length: int,
         width: int,
         mA_cm2: int,
-        voltage_per_cell: int,
+        voltage_per_cell: float,
         number_of_cells: int,
     ):
         """This program calculates the total power of a battery pack based on the number of cells in series or parallel.
@@ -30,20 +30,25 @@ class CalculateBatteryConnections:
 
     def __calculate_area(self):
         area = self.length * self.width
+        print(f"Area of the battery pack is {area} cm2")
         return area
 
-    def __calculate_cell_power(self):
+    def __calculate_cell_amps(self):
         total_power = self.__calculate_area() * self.mA_cm2
+        total_power /= 1000
+        print(f"Total power of the battery pack is {total_power} A")
         return total_power
 
     def __calculate_series(self):
         series_voltage = self.voltage_per_cell * self.number_of_cells
-        series_power = self.__calculate_cell_power() * series_voltage
+        series_power = self.__calculate_cell_amps() * series_voltage
+        print(f"Total power of the battery pack is {series_power} W")
         return series_power
 
     def __calculate_parallel(self):
-        parallel_capacity = self.__calculate_cell_power() * self.number_of_cells
+        parallel_capacity = self.__calculate_cell_amps() * self.number_of_cells
         parallel_power = parallel_capacity * self.voltage_per_cell
+        print(f"Total power of the battery pack is {parallel_power} W")
         return parallel_power
 
     # create a function to calculate the total power of the battery pack using the series and parallel functions based on the number of cells
@@ -57,12 +62,12 @@ class CalculateBatteryConnections:
     def display(self, parallel: bool):
         total_power = self.__calculate_total_power(parallel)
         if total_power > 1000:
-            total_power = total_power / 1000
+            total_power /= 1000
             print(f"Total power of the battery pack is {total_power} kW")
             return
         print(f"Total power of the battery pack is {total_power} W")
 
 
 if __name__ == "__main__":
-    battery = CalculateBatteryConnections(10, 10, 215, 1.25, 50)
-    battery.display(False)
+    battery = CalculateBatteryConnections(17, 8, 215, 1.25, 50)
+    battery.display(True)
